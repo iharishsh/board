6ebeb7aef0bcf492c981254acb351a8d6be59c8c
iharishsh <iharishsh@gmail.com>
2024-08-14 06:01:12 +0530

    AuthLoading, Clerk Userbutton

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -2,14 +2,11 @@
 
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import {
-    AuthLoading,
-    Authenticated,
-    ConvexReactClient
-} from "convex/react";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import Loading from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
@@ -17,13 +14,16 @@ const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
 const convex = new ConvexReactClient(convexUrl);
 
 export const ConvexClientProvider = ({
-    children,
+  children,
 }: ConvexClientProviderProps) => {
-    return (
-        <ClerkProvider>
-            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-                {children}
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    );
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 };
