64984a3d5e1bcd71dbf160dd21e3c4cfe265db46
iharishsh <iharishsh@gmail.com>
2024-08-19 21:05:32 +0530

    card actions - ui

---
+++ components/actions.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { 
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+interface ActionProps {
+    children: React.ReactNode;
+    side?: DropdownMenuContentProps["side"];
+    sideOffset?: DropdownMenuContentProps["sideOffset"];
+    id: string;
+    title: string;
+};
+
+export const Actions = ({
+    children,
+    side,
+    sideOffset,
+    id,
+    title,
+}: ActionProps) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                {children}
+            </DropdownMenuTrigger>
+        </DropdownMenu>
+    )
+}
