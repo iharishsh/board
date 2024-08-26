bdf07a459f5117f3393e0eab3f2d3bc7169fbbaa
iharishsh <iharishsh@gmail.com>
2024-08-26 19:46:42 +0530

    Canvas State

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { useCanRedo, useCanUndo, useHistory } from "@liveblocks/react/suspense";
 
 interface CanvasProps {
     boardId: string;
@@ -11,14 +14,28 @@ interface CanvasProps {
 export const Canvas = ({
     boardId,
 }: CanvasProps) => {
-    
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
+
+    const history = useHistory(); 
+    const canUndo = useCanUndo(); 
+    const canRedo = useCanRedo(); 
+
     return (
         <main 
             className="h-full w-full relative bg-neutral-100 touch-none"
         >
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar 
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
         </main>
     );
 };
