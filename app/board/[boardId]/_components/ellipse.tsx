959f8a25f35f351e693e5f6d00a76507322c58ed
iharishsh <iharishsh@gmail.com>
2024-09-01 13:54:53 +0530

    Elements - Ellipse & Text

---
+++ app/board/[boardId]/_components/ellipse.tsx
@@ -0,0 +1,30 @@
+import { colorToCss } from "@/lib/utils";
+import { EllipseLayer } from "@/types/canvas";
+
+interface EllipseProps {
+  id: string;
+  layer: EllipseLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Ellipse = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: EllipseProps) => {
+  return (
+    <ellipse
+      className="drop-shadow-md"
+      onPointerDown={(e) => onPointerDown(e, id)}
+      cx={layer.x + layer.width / 2}  
+      cy={layer.y + layer.height / 2}
+      rx={layer.width / 2}
+      ry={layer.height / 2}
+      fill={layer.fill ? colorToCss(layer.fill) : "#000"}
+      stroke={selectionColor || "transparent"}
+      strokeWidth="1"
+    />
+  );
+};
