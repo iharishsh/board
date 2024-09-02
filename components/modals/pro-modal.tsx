d13e8a9f5d4620c6a35b7a42712bf87a72b61acb
iharishsh <iharishsh@gmail.com>
2024-09-02 20:23:02 +0530

    Pro Modal UI

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+
+
 import { 
     Dialog,
     DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
 } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
 import { useProModal } from "@/store/use-pro-modal";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"]
+})
+
 export const ProModal = () => {
     const {
         isOpen,
@@ -16,15 +25,34 @@ export const ProModal = () => {
 
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>
-                        Pro Modal
-                    </DialogTitle>
-                </DialogHeader>
-                <DialogDescription>
-                    This is pro modal
-                </DialogDescription>
+            <DialogContent className="max-w-[340px] p-0 overflow-hidden">
+                <div className="aspect-video relative flex items-center justify-center">
+                    <Image 
+                        src="/pro.svg"
+                        alt="Pro"
+                        className="object-fit"
+                        fill
+                    />
+                </div>
+                <div className={cn(
+                    "text-neutral-700 mx-auto space-y-6 p-6",
+                    font.className,
+                )}>
+                    <h2 className="font-medium text-lg">
+                        🚀Upgrade to Pro today!
+                    </h2>
+                    <div className="pl-3">
+                        <ul className="text-[11px] space-y-1 list-disc">
+                            <li>Unlimited boards</li>
+                            <li>Unlimited tools</li>
+                            <li>Unlimited organizations</li>
+                            <li>Unlimited members</li>
+                        </ul>
+                    </div>
+                    <Button size="sm" className="w-full">
+                        Upgrade
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     );
