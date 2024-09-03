c3bb2181f105b790fdc1f92488547f543f972b25
iharishsh <iharishsh@gmail.com>
2024-09-03 06:34:38 +0530

    Stripe Webhook

--- convex/stripe.ts
+++ convex/stripe.ts
@@ -3,7 +3,7 @@
 import Stripe from "stripe";
 import { v } from "convex/values";
 
-import { action } from "./_generated/server";
+import { action, internalAction } from "./_generated/server";
 
 const url = process.env.NEXT_PUBLIC_APP_URL;
 const stripe = new Stripe(
@@ -33,7 +33,7 @@ export const pay = action({
             line_items: [
                 {
                     price_data: {
-                        currency: "INR",
+                        currency: "USD",
                         product_data: {
                             name: "Board Pro",
                             description: "Unlimited boards for your organization",
@@ -53,5 +53,31 @@ export const pay = action({
         });
 
         return session.url!;
+    }
+});
+
+export const fulfill = internalAction({
+    args: { signature: v.string(), payload: v.string() },
+    handler: async (ctx, { signature, payload }) => {
+        const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+
+        try {
+            const event = stripe.webhooks.constructEvent(
+                payload,
+                signature,
+                webhookSecret
+            );
+
+            const session = event.data.object as Stripe.Checkout.Session;
+
+            if (event.type === "checkout.session.completed") {
+                console.log("CHECKOUT_COMPLETED");
+            }
+
+            return { success: true };
+        } catch (err) {
+            console.error(err);
+            return { success: false };
+        }
     }
 });
